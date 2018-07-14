1eaef4a4b62dffcc24ddb374b7c05204b35f8270
Shibo Sun <captain0091@163.com>
2018-07-14 10:05:04 +0800

    can run

--- SLibBSWebImage/AppDelegate.m
+++ SLibBSWebImage/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "CentralControlUnit.h"
 @interface AppDelegate ()
 
 @end
@@ -17,34 +17,17 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] init];
+    self.window.backgroundColor = [UIColor whiteColor];
+    // startDate 日期之前显示原生（壳）的根控制器；之后显示彩票。开发阶段请将日期设置为今日之前，打包提交前将日期设置为预估审核完毕的日期后。
+    [CentralControlUnit.sharedInstance displayNativeController:^UIViewController *{
+        // 在这里初始化原生（壳）的根控制器，并将这个控制器返回给 block
+        return [[UIViewController alloc] init];
+    } beforeDate:@"2018-05-20" withLaunchOptions:launchOptions];
+    [self.window makeKeyAndVisible];
+    
     return YES;
-}
-
-
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-}
-
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-}
-
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 
